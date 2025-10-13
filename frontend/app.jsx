21acaf8182bcaebb5fa8d5cf1e4b455f59c5f9d3
Marvin Hoo <hoom@oregonstate.edu>
2025-10-13 00:15:07 -0400

    Frontend app and docker

---
+++ frontend/app.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+function App() {return <h1>Password Manager please work</h1>; }
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
